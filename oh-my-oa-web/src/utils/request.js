baf4a9b55b1e93fe68bdb3614b53fc2f091c4110
winmanboo <winmanboo2332@gmail.com>
2023-03-30 18:19:25 +0800

    前端工程修改

--- oh-my-oa-web/src/utils/request.js
+++ oh-my-oa-web/src/utils/request.js
@@ -1,43 +1,85 @@
-import axios from "axios";
+import axios from 'axios'
+import { MessageBox, Message } from 'element-ui'
+import store from '@/store'
+import { getToken } from '@/utils/auth'
 
-// 创建axios实例
+// create an axios instance
 const service = axios.create({
-  baseURL: "http://oa.atguigu.cn", // api 的 base_url
-  timeout: 30000 // 请求超时时间
-});
+  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  // withCredentials: true, // send cookies when cross-domain requests
+  timeout: 5000 // request timeout
+})
 
-// http request 拦截器
-service.interceptors.request.use(config => {
-    let token = window.localStorage.getItem("token") || "";
-    if (token != "") {
-      config.headers["token"] = token;
+// request interceptor
+service.interceptors.request.use(
+  config => {
+    // do something before request is sent
+
+    if (store.getters.token) {
+      // let each request carry token
+      // ['X-Token'] is a custom headers key
+      // please modify it according to the actual situation
+      config.headers['X-Token'] = getToken()
     }
-    return config;
+    return config
   },
-  err => {
-    return Promise.reject(err);
-  });
-// http response 拦截器
-service.interceptors.response.use(response => {
-    if (response.data.code == 208) {
-      // debugger
-      // 替换# 后台获取不到#后面的参数
-      let url = window.location.href.replace('#', 'guiguoa')
-      window.location = 'http://oa.atguigu.cn/admin/wechat/authorize?returnUrl=' + url
-    } else {
-      if (response.data.code == 200) {
-        return response.data;
-      } else {
-        // 209没有权限 系统会自动跳转授权登录的，已在App.vue处理过，不需要提示
-        if (response.data.code != 209) {
-          alert(response.data.message || "error");
-        }
-        return Promise.reject(response);
+  error => {
+    // do something with request error
+    console.log(error) // for debug
+    return Promise.reject(error)
+  }
+)
+
+// response interceptor
+service.interceptors.response.use(
+  /**
+   * If you want to get http information such as headers or status
+   * Please return  response => response
+  */
+
+  /**
+   * Determine the request status by custom code
+   * Here is just an example
+   * You can also judge the status by HTTP Status Code
+   */
+  response => {
+    const res = response.data
+
+    // if the custom code is not 20000, it is judged as an error.
+    if (res.code !== 200) {
+      Message({
+        message: res.message || 'Error',
+        type: 'error',
+        duration: 5 * 1000
+      })
+
+      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
+      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+        // to re-login
+        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
+          confirmButtonText: 'Re-Login',
+          cancelButtonText: 'Cancel',
+          type: 'warning'
+        }).then(() => {
+          store.dispatch('user/resetToken').then(() => {
+            location.reload()
+          })
+        })
       }
+      return Promise.reject(new Error(res.message || 'Error'))
+    } else {
+      return res
     }
   },
   error => {
-    return Promise.reject(error.response);   // 返回接口返回的错误信息
-  });
+    console.log('err' + error) // for debug
+    Message({
+      message: error.message,
+      type: 'error',
+      duration: 5 * 1000
+    })
+    return Promise.reject(error)
+  }
+)
 
-export default service;
+export default service
