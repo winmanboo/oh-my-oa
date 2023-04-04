5cc00515c47d0dd918ff6defc52b183a5701714c
winmanboo <winmanboo2332@gmail.com>
2023-04-04 19:07:14 +0800

    上传流程定义接口

--- service-oa/src/main/java/com/winmanboo/oh_my_oa/process/controller/OaProcessTemplateController.java
+++ service-oa/src/main/java/com/winmanboo/oh_my_oa/process/controller/OaProcessTemplateController.java
@@ -3,11 +3,20 @@ package com.winmanboo.oh_my_oa.process.controller;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.winmanboo.common.result.Result;
+import com.winmanboo.common.result.ResultCodeEnum;
 import com.winmanboo.model.process.ProcessTemplate;
 import com.winmanboo.oh_my_oa.process.service.OaProcessTemplateService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.util.ResourceUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * <p>
@@ -63,5 +72,37 @@ public class OaProcessTemplateController {
   public Result<Void> remove(@PathVariable Long id) {
     processTemplateService.removeById(id);
     return Result.ok();
+  }
+
+  @ApiOperation("上传流程定义")
+  @PostMapping("/uploadProcessDefinition")
+  public Result<?> uploadProcessDefinition(MultipartFile file) throws FileNotFoundException {
+    // 获取 classes 目录位置
+    String path = new File(ResourceUtils.getURL("classpath:").getPath()).getAbsolutePath();
+
+    String fileName = file.getOriginalFilename();
+    if (!StringUtils.hasText(fileName))
+      return Result.build(null, ResultCodeEnum.FAIL).message("文件名不存在");
+
+    // 上传目录
+    File tempfile = new File(path + "/processes/");
+    // 判断目录是否存着
+    if (!tempfile.exists()) {
+      tempfile.mkdirs();
+    }
+    // 创建空文件用于写入文件
+    File zipFile = new File(path + "/processes/" + fileName);
+    // 保存文件流到本地
+    try {
+      file.transferTo(zipFile);
+    } catch (IOException e) {
+      e.printStackTrace();
+      return Result.build(null, ResultCodeEnum.FAIL).message("上传失败");
+    }
+    // 根据上传地址后续部署流程定义，文件名称为流程定义的默认 key
+    return Result.ok(Map.of(
+        "processDefinitionPath", "processes/" + fileName,
+        "processDefinitionKey", fileName.substring(0, fileName.lastIndexOf("."))
+    ));
   }
 }
