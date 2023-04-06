54d85a805c26db7e38d59cc9ec5555e22e22bab3
winmanboo <winmanboo2332@gmail.com>
2023-04-06 22:32:50 +0800

    获取审批模版数据接口

--- service-oa/src/main/java/com/winmanboo/oh_my_oa/process/controller/api/ProcessController.java
+++ service-oa/src/main/java/com/winmanboo/oh_my_oa/process/controller/api/ProcessController.java
@@ -1,15 +1,14 @@
 package com.winmanboo.oh_my_oa.process.controller.api;
 
 import com.winmanboo.common.result.Result;
+import com.winmanboo.model.process.ProcessTemplate;
 import com.winmanboo.model.process.ProcessType;
+import com.winmanboo.oh_my_oa.process.service.OaProcessTemplateService;
 import com.winmanboo.oh_my_oa.process.service.OaProcessTypeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +19,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProcessController {
   private final OaProcessTypeService processTypeService;
+
+  private final OaProcessTemplateService processTemplateService;
+
+  @ApiOperation("获取审批模版数据")
+  @GetMapping("/getProcessTemplate/{processTemplateId}")
+  public Result<ProcessTemplate> getProcessTemplate(@PathVariable Long processTemplateId) {
+    return Result.ok(processTemplateService.getById(processTemplateId));
+  }
 
   @ApiOperation("查询所有的审批分类和每个分类所有的审批模版")
   @GetMapping("/findProcessType")
