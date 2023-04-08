c189c75e44464bca2012e26987261bf56ff0912c
winmanboo <winmanboo2332@gmail.com>
2023-04-08 13:18:05 +0800

    请求响应判断是否token过期

--- oh-my-oa-back/src/utils/request.js
+++ oh-my-oa-back/src/utils/request.js
@@ -54,11 +54,12 @@ service.interceptors.response.use(
       })
 
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+      // if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+      if (res.code === 205 || res.code === 208) {
         // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
+        MessageBox.confirm('您已经登出，您可以取消留在该页面，或重新登录', '确认登出', {
+          confirmButtonText: '重新登陆',
+          cancelButtonText: '取消',
           type: 'warning'
         }).then(() => {
           store.dispatch('user/resetToken').then(() => {
